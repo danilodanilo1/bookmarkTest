39f3a28b5c3b6edc970431fef8f15fb32dee8049
danilodanilo1 <danilo.onascimento@hotmail.com>
2022-12-26 14:03:08 -0300

    add nome

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import LogoImage from '../../assets/images/logo.svg'
-import { LogoImg } from './styles';
-import * as S from './styles.js'
+import React from "react";
+import LogoImage from "../../assets/images/logo.svg";
+import { LogoImg } from "./styles";
+import * as S from "./styles.js";
 
 // import { Container } from './styles';
 
@@ -10,7 +10,7 @@ function Logo() {
     <S.Header>
       <S.LogoImg src={LogoImage} />
       <h1>Travel Book Agenda.</h1>
-      <h2>Here you can book all your travels very simple.</h2>
+      <h2>Your travels very simple. By Danilo Nascimento</h2>
     </S.Header>
   );
 }
